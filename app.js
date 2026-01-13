9669ab3a7562d49657e2376e25ee4dd16577d7b1
JEONGWOO LEE <ljwljw8541@uos.ac.kr>
2026-01-13 15:53:55 +0900

    refactor: Redis와 BullMQ 의존성 삭제

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 import express from 'express';
-import { addJobs } from './queue.js';
 
 const app = express();
 app.use(express.json());
 app.listen(3000);
 
-
-app.post('/api/jobs', async (req, res) => { // 1. 요청
+app.post('/api/run', async (req, res) => {
     const { code, input } = req.body;
-    const job = await addJobs(code, input); // 2. job 생성 및 MQ에 job push
-
-    console.log(code, input);
-
-    res.status(202).json({ // 3. job.id 반환
-        jobId: job.id,
-        status: 'queued',
+    
+    // TODO: Python 코드 실행 로직 구현
+    // 1. Sandbox 생성
+    // 2. 코드 실행
+    // 3. 결과 반환
+    
+    res.json({
+        stdout: '',
+        stderr: '',
+        exitCode: 0,
     });
-
 });
-
-// 8. jobId로 Redis 조회
-// 9. 상태/결과 반환
 
 
